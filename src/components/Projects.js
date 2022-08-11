920cc85150b6ede5bbb876eeca972640dc3b25fd
dimstry <dimastriana03@gmail.com>
2022-08-11 20:50:11 +0700

    add New Project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -101,21 +101,32 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                    <Row>
-                        {
-                          projects2.map((project, index) => {
-                            return (
-                              <ProjectCard     
-                                  key={index}
-                                  {...project}
-                                  />
-                            )
-                          })
-                        }
+                      <Row>
+                          {
+                            projects2.map((project, index) => {
+                              return (
+                                <ProjectCard     
+                                    key={index}
+                                    {...project}
+                                    />
+                              )
+                            })
+                          }
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    <Row>
+                          {
+                            projects3.map((project, index) => {
+                              return (
+                                <ProjectCard     
+                                    key={index}
+                                    {...project}
+                                    />
+                              )
+                            })
+                          }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
