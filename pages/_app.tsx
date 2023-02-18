bcbe99ea522263985d551955ec438ebad201b6db
dimstry <dimastriana03@gmail.com>
2023-02-18 09:06:54 +0700

    minify css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-title-in-document-head */
 import "styles/globals.css";
+import "styles/main.css";
 import "animate.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
