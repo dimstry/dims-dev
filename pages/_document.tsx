d43b4a11cd8a9c692650961e6dc2ae7e956513a7
dimstry <dimastriana03@gmail.com>
2023-02-20 09:17:25 +0700

    add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
 
@@ -5,12 +6,26 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="Dims | Dev" />
         <meta name="theme-color" content="#000000" />
         <link
           href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet"
         ></link>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-10BMJ9GXM3"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-10BMJ9GXM3');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
